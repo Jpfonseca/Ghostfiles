c9dd4e2a63dc5aa2cc6879eae5e7181c12b54de0
João Pedro Fonseca <jpedrofonseca@outlook.pt>
2020-09-01 13:02:53 +0100

    Added latest rice config

--- sway/scripts/swaybar.sh
+++ sway/scripts/swaybar.sh
@@ -13,7 +13,12 @@ battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "
 sound_status=$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }')
 sound_volume=$(pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
 
-#Wifi status
+##Ethernet Status
+ethernet_bitrate=$(dmesg| grep enp2s0 | grep Up |awk '{print $10}' | head -n 1)
+ethernet_interface=$(dmesg| grep enp2s0 | grep Up |awk '{print $5}' | head -n 1)
+ethernet_status=$(cat /sys/class/net/enp2s0/operstate)
+
+#Wifi Status
 wifi_bitrate=$(iwconfig wlan0 |grep "Bit Rate"| awk '{print $2 $3}'|cut -d \= -f 2 ) 
 wifi_channel=$(iwconfig wlan0 |grep "Frequency"| awk '{print $2 $3}'|cut -d \: -f 2)
 wifi_ap=$(iwconfig wlan0 |grep "ESSID"| cut -d ':' -f 2)
@@ -39,14 +44,21 @@ else
     sound_status='🔊'
 fi
 
-##Wifi status
+##Wifi Status
 if [ $wifi_ap = "off/any" ];
 then
     wifi_ap="disconnected"
     wifi_channel=""
 fi
 
+##Ethernet Status
+if [ $ethernet_status = "down" ];
+then
+    ethernet_status="disconnected"
+    ethernet_bitrate=""
+fi
+
 #Output
 
-echo "🖴 $disk_usage  | 📶$wifi_ap $wifi_bitrate $wifi_channel | $sound_status $sound_volume% | $charging_status $battery_charge | 🕘 $current_date "
+echo "🖴 $disk_usage | 🖧 $ethernet_interface $ethernet_status $ethernet_bitrate  | 📶$wifi_ap $wifi_bitrate $wifi_channel | $sound_status $sound_volume% | $charging_status $battery_charge | 🕘 $current_date "
 
