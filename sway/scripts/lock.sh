098dd0a6e479db89c8e0ef8a8ae0b3ee2da0e32a
João Pedro Fonseca <jpedrofonseca@ua.pt>
2025-02-02 18:04:02 +0000

    added new configs

---
+++ sway/scripts/lock.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+swaylock \
+--screenshots \
+--effect-blur 7x5 \
+--fade-in 0.2 \
+-u
--- sway/scripts/swaybar.sh
+++ sway/scripts/swaybar.sh
@@ -4,19 +4,21 @@
 current_date=$(date "+%Y-%m-%d %H:%M:%S %p")
 
 ##Battery
-battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "percentage" | awk '{print $2}')
-battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}')
+battery_charge=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}')
+battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "state" | awk '{print $2}')
 
 ##Sound Status
 # https://unix.stackexchange.com/a/561305
 #sound_status=$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }'| awk 'NR>0')
-sound_status=$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }')
-sound_volume=$(pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
+sound_status=$(pactl list sinks |grep Mute| awk '/Mute:/{print $2}') #$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }')
+sound_volume=$(pactl list sinks |grep Volume|awk '/Volume: front-left:/{ print $5}'| sed 's/[%|,]//g') #$(pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g')
+mic_status=$(pactl list sink-inputs |grep Mute| awk '/Mute:/{print $2}') #$(pacmd list-sinks|grep -A 15 '* index'|awk '/muted:/{ print $2 }')
+
 
 ##Ethernet Status
-ethernet_bitrate=$(dmesg| grep enp2s0 | grep Up |awk '{print $10}' | head -n 1)
-ethernet_interface=$(dmesg| grep enp2s0 | grep Up |awk '{print $5}' | head -n 1)
-ethernet_status=$(cat /sys/class/net/enp2s0/operstate)
+ethernet_bitrate=$(cat /sys/class/net/enp0s31f6/speed)
+ethernet_interface=enp0s31f6
+ethernet_status=$(cat /sys/class/net/enp0s31f6/operstate)
 
 #Wifi Status
 wifi_bitrate=$(iwconfig wlan0 |grep "Bit Rate"| awk '{print $2 $3}'|cut -d \= -f 2 ) 
@@ -37,11 +39,18 @@ else
 fi
 
 ##Sound Status
-if [ $sound_status = "yes" ];
+if grep -q "yes" <<< "$sound_status";
 then
     sound_status='🔇'
 else
     sound_status='🔊'
+fi
+
+if [[ $mic_status == "yes" ]];
+then
+    mic_status='🙊'
+else
+    mic_status='🎤'
 fi
 
 ##Wifi Status
@@ -60,5 +69,5 @@ fi
 
 #Output
 
-echo "🖴 $disk_usage | 🖧 $ethernet_interface $ethernet_status $ethernet_bitrate  | 📶$wifi_ap $wifi_bitrate $wifi_channel | $sound_status $sound_volume% | $charging_status $battery_charge | 🕘 $current_date "
+echo "🖴 $disk_usage | 🖧 $ethernet_interface $ethernet_status $ethernet_bitrate  | 📶$wifi_ap $wifi_bitrate $wifi_channel | $mic_status | $sound_status $sound_volume% | $charging_status $battery_charge | 🕘 $current_date "
 
